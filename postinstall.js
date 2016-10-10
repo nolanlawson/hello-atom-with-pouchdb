9260a25b79f7f9710e411217596417546d310989
ammar-hasan <ammar_rizvi@msn.com>
2016-10-10 15:45:27 +0500

    update

--- postinstall.js
+++ postinstall.js
@@ -18,11 +18,11 @@ var exec = require('child_process').exec,
 try {
   process.chdir('node_modules/leveldown');
 
-  exec(cmd, (error, stdout, stderr) => {
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+  exec(cmd, function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
     if (error !== null) {
-      console.log(`exec error: ${error}`);
+      console.log('exec error: ' + error);
     }
   });
 } catch (err) {
